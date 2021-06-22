2113f0f1eb872ae03679b9abed99ab277bc63b69
ehmusman <ehmusman@gmail.com>
2021-06-22 23:36:39 +0500

    objects section is started

---
+++ 02-objects/01-intro.js
@@ -0,0 +1,9 @@
+// Javascript is all about objects. before learning object oriented programming we have to get a good understanding of objects.
+// we'll look at
+// Creating Objects
+// Factories and Constructors
+// Primitive and Reference Types
+// Working with properties of an object such as adding, removing, properties as well as enumerating them. 
+// how to define private properties
+// Getters/Setters
+
