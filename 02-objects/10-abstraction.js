775dae0ceb10b919bd4d6f19b684aca758cf7a86
ehmusman <ehmusman@gmail.com>
2021-06-23 17:47:32 +0500

    abstraction is implemented

--- 02-objects/10-abstraction.js
+++ 02-objects/10-abstraction.js
@@ -1,20 +1,20 @@
 // now lets introduce some complexity in this example.
 // lets add a property and method in the object
 
-function Circle(radius) {
-    this.radius = radius
-    this.defaultLocarion = { x: 0, y: 0 }
-    this.computeOptimumLocation = function () {
-        // ...
-    }
-    this.draw = function () {
-        // let call this function here
-        this.computeOptimumLocation()
-        console.log("draw")
-    }
-}
+// function Circle(radius) {
+//     this.radius = radius
+//     this.defaultLocarion = { x: 0, y: 0 }
+//     this.computeOptimumLocation = function () {
+//         // ...
+//     }
+//     this.draw = function () {
+//         // let call this function here
+//         this.computeOptimumLocation()
+//         console.log("draw")
+//     }
+// }
 
-const circle = new Circle(1)
+// const circle = new Circle(1)
 
 // accidently if we run 
 // circle.defaultLocarion = false // it may be dangerous for the object
@@ -32,3 +32,48 @@ const circle = new Circle(1)
 /// many developers are not able to implement the OOP in a good way. their all properties are exposes a little bit change will lead to modify a lot of code.
 // so keep in mind the abstraction principle. that is hide the details and expose the essentials.
 
+
+
+///////////////////////////////////////////////////// Abstraction implementation
+
+function Circle(radius) {
+    // let color = "red";
+
+    this.radius = radius
+    let defaultLocation = { x: 0, y: 0 }
+
+    let computeOptimumLocation = function () {
+        // ...
+        console.log("hello world")
+    }
+    this.draw = function () {
+        // let call this function here
+        computeOptimumLocation()
+        console.log(defaultLocation)
+        console.log("draw")
+    }
+}
+
+const circle = new Circle(1)
+console.log(circle.draw())
+
+// so how we can implemet abstraction here?
+// if we initialize the local variable inside the function? it will not be the part of the created object.
+// this will be only the part of the object when we use this.color = color
+// so in this way we can easily hide a certain members from the outside.
+
+
+// we'll initialize the defaultLocation property and computeOptimumLocation method with a let keyword.
+ // now this is no longer the part of the object and we can not acces the method wit hthis keyword.
+ // hers we can access it directly here. due to the concept of Closure in javascript.
+
+
+ // let we initialize the variable let x,y inside the function. these variables have a limited scope. they are go out of scope when we finish the function execution.
+ // a closure determines that what variables are accessible to the inner functions.
+ // inner function can acces the variables that are initialized inside its scope and it can also access the variables that are defined in its parent function.
+
+ // dont confuse scope and closur
+ // the scope is temporary and it will die
+ // every time we call the draw method, these variablkes are recreated and re initialized and after that function they will die. so scope is temporary and closure stays there.
+
+ // when we run the draw function after its execution the variables defaultLocation , computeOptimumLocation will stay in the memory to preserve their there state.
